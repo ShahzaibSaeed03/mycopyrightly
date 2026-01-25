22013685de32bb8859730486ee615b28d95e11d6
Shahzaib Saeed <shahzaibsaeed077@gmail.com>
2026-01-25 21:07:24 +0500

    update api

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: "https://localhost:7117/api",
+    apiUrl: "https://copyrightlybackend-jvnks.ondigitalocean.app/api",
     stripePublishAbleKey: 'pk_test_51R4haHLIxobea5p9MB1Po7qydXzckU1Xnv5PIgvDoEYnp9CwT4UEm23xpegbORk4lELoAUH3ietslmpte7GqrXJ300BeDPsC0l'
   };
